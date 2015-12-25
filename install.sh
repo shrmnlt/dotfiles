7d175277317e0c2807ac9713dd0d0be6233d29e1
hrmnlt <hrmnlt@outlook.com>
2015-12-25 12:22:23 +0200

    Add symlinking of .bashrc, .bash_profile to $HOME dir;

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # brew essential tools
-brew install curl git python
+# brew install curl git python
 
 # bootstrap dev apps
-brew cask install atom
-brew cask install transmission
-brew cask install virtualbox
-brew cask install atom
+# brew cask install atom
+# brew cask install transmission
+# brew cask install virtualbox
+# brew cask install atom
 
 # Install git-completion
 GIT_COMPLETION_URL="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
@@ -14,5 +14,9 @@ if ! curl "$GIT_COMPLETION_URL" --output "$HOME/.dotfiles/.git-completion.bash";
   echo "Error! Can't download git-completion" && exit 1
 fi
 
+
+
 # Symlink .inputrc
+ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.dotfiles/.bash_profile" "$HOME/.bash_profile"
 ln -s "$HOME/.dotfiles/system/.inputrc" "$HOME/.inputrc"
